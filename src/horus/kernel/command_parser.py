39bcb5578a5864479c502d70a8937c41a1473924
Philipp Rosenfeld <philipp.rosenfeld@verbund.at>
2026-07-28 11:00:22 +0200

    Implemented rough command dispatching structure

---
+++ src/horus/kernel/command_parser.py
@@ -0,0 +1,18 @@
+import argparse
+
+class CommandArgumentParser(argparse.ArgumentParser):
+
+    def exit(self, status: int, message: str| None = None) -> None:
+        raise CommandParseError(message or "", self.format_usage())
+
+    def error(self, message: str) -> None:
+        raise CommandParseError(message, self.format_usage())
+
+
+
+class CommandParseError(Exception):
+
+    def __init__(self, message: str, usage : str) -> None:
+        self.message = message
+        self.usage = usage
+        super().__init__(message)
